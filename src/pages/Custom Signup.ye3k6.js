4d0e6e131c5bff1a6bbae0167b2deb920a954096
ANNS <cam@anns.ai>
2023-09-23 01:17:57 -0400

    ayyy

--- src/pages/Custom Signup.ye3k6.js
+++ src/pages/Custom Signup.ye3k6.js
@@ -1,13 +1,31 @@
-
 import { authentication } from "wix-members-frontend";
 $w.onReady(function () {
     $w('#loginbox').onMessage((event) => {
         let email = event.data.email;
         let password = event.data.password;
         authentication.login(email, password)
+            .then(() => {
+                $w('#lightbox1').collapse();
+            })
             .catch(() => {
                 authentication.register(email, password)
                     .then(() => {
+                        $w('#lightbox1').collapse();
+                        return authentication.login(email, password);
+                    })
+            })
+    });
+    $w('#googlebox').onMessage((event) => {
+        let email = event.data.getEmail();
+        let password = event.data.getId();
+        authentication.login(email, password)
+            .then(() => {
+                $w('#lightbox1').collapse();
+            })
+            .catch(() => {
+                authentication.register(email, password)
+                    .then(() => {
+                        $w('#lightbox1').collapse();
                         return authentication.login(email, password);
                     })
             })
